e3c50d74bfe3a61f21e2a40142d3e2156d6ac00b
Jokey467 <nechai.vtz21@gmail.com>
2024-10-19 19:32:40 +0300

    lab

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,17 +1,11 @@
-
-public class Main {
+public class Lab1 {
     public static void main(String[] args) {
-
         double a = 0.58;
         double b = 0.34;
         double c = 1.25;
         double d = -1.89;
-
         double y = 2 * (Math.sin(a) / Math.acos(-2 * b)) - Math.sqrt(Math.log(c * Math.abs(2 * d)));
-
-        System.out.println("Значение y: " + y);
-
-
+        System.out.println("y: " + y);
     }
 }
 
